f7fa4196ac74a850288b8013cad36a0e01303a81
Jordan & Andrew Lee <student@epicodus.com>
2015-05-13 14:58:45 -0700

    add a recipe page to the program

--- app.rb
+++ app.rb
@@ -9,6 +9,27 @@ get('/') do
 end
 
 get('/recipes/all') do
-  @recipies = Recipe.all()
+  @recipes = Recipe.all()
   erb(:all_recipes)
+end
+
+post('/recipes/all') do
+  recipe_name = params.fetch('recipe_name')
+  new_recipe = Recipe.create({:name => recipe_name})
+  @recipes = Recipe.all()
+  erb(:all_recipes)
+end
+
+get('/recipe/:id') do
+  @recipe = Recipe.find(params.fetch('id').to_i())
+  erb(:add_to_recipe_form)
+end
+
+post('/recipe/:id') do
+  @recipe = Recipe.find(params.fetch('id').to_i())
+  recipe_rating = params.fetch('recipe_rating')
+  recipe_ingredients = params.fetch('recipe_ingredients')
+  recipe_instructions = params.fetch('recipe_instructions')
+  @recipe.update({:rating => recipe_rating, :ingredients => recipe_ingredients, :instructions => recipe_instructions})
+  erb(:add_to_recipe_form)
 end
