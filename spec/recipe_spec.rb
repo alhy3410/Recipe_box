370e3efc4b60b98ce4c5b00cc79445b50dda2500
Jordan & Andrew Lee <student@epicodus.com>
2015-05-13 13:40:56 -0700

    add second spec to test categories in recipe class. spec passes

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -1,4 +1,12 @@
 require('spec_helper')
 
 describe(Recipe) do
+  describe('#categories') do
+    it('has many categories') do
+      test_recipe = Recipe.create(:name => "Pizza")
+      category1 = test_recipe.categories.create(:name => "Breakfast")
+      category2 = test_recipe.categories.create(:name => "Dinner")
+      expect(test_recipe.categories()).to(eq([category1, category2]))
+    end
+  end
 end
